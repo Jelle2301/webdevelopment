c14df9e14db0af177e1933e78fc85e2f05bcc7b9
Jelle2301 <146099496+Jelle2301@users.noreply.github.com>
2024-04-18 19:31:34 +0200

    labo 20 versie 2

--- labo20/start contact manager opdracht/scripts/code.js
+++ labo20/start contact manager opdracht/scripts/code.js
@@ -1,4 +1,26 @@
-let personen = [];
+let personen=[
+    {
+        voornaam: 'Jan',
+        familienaam: 'Janssens',
+        geboorteDatum: new Date('2010-10-10'),
+        email: 'jan@example.com',
+        aantalKinderen: 0
+    },
+    {
+        voornaam: 'Mieke',
+        familienaam: 'Mickelsen',
+        geboorteDatum: new Date('1980-01-01'),
+        email: 'mieke@example.com',
+        aantalKinderen: 1
+    },
+    {
+        voornaam: 'Piet',
+        familienaam: 'Pieters',
+        geboorteDatum: new Date('1970-12-31'),
+        email: 'piet@example.com',
+        aantalKinderen: 2
+    }
+];
 // Event listener (btnBewaar click)
 // Bewaar de wijzigingen die in de user interface werden aangebracht
 const bewaarBewerktePersoon = () => {
@@ -27,7 +49,10 @@ const bewaarBewerktePersoon = () => {
     }
     if (!bestaatPersoon) {
         personen.push(persoon);
-        personenHTML.innerHTML = `<option value="${persoon.voornaam}  ${persoon.familienaam}">${persoon.voornaam}  ${persoon.familienaam}</option>`;
+        let option = document.createElement("option");
+        option.textContent = persoon.voornaam + ' ' + persoon.familienaam;
+        option.value = persoon.voornaam + ' ' + persoon.familienaam;
+        personenHTML.appendChild(option);
     }
     // een bestaande persoon in de lijst passen we aan
     // zorg ervoor dat de naam en voornaam ook aangepast en/of zichtbaar zijn in de lijst na updaten
@@ -53,7 +78,6 @@ const bewaarBewerktePersoon = () => {
     }
 
 };
-
 // Event listener (btnNieuw click)
     const bewerkNieuwePersoon = () => {
         console.log("Klik op de knop nieuw");
@@ -64,7 +88,24 @@ const bewaarBewerktePersoon = () => {
         document.getElementById("txtEmail").value = "";
         document.getElementById("txtAantalKinderen").value = "";
     };
+    const klikOptie = (event) => {
+        let persoon = event.target.selectedIndex;
 
+        document.getElementById("txtVoornaam").value = personen[persoon].voornaam;
+        document.getElementById("txtFamilienaam").value = personen[persoon].familienaam;
+        document.getElementById("txtGeboorteDatum").value = personen[persoon].geboorteDatum.toLocaleDateString('en-CA');
+        document.getElementById("txtEmail").value = personen[persoon].email;
+        document.getElementById("txtAantalKinderen").value = personen[persoon].aantalKinderen;
+    }
+    const showList = () => {
+        let lstPersonen = document.getElementById("lstPersonen");
+        for(let i = 0; i < personen.length; i++){
+            let option = document.createElement("option");
+            option.textContent = personen[i].voornaam + ' ' + personen[i].familienaam;
+            option.value = personen[i].voornaam + ' ' + personen[i].familienaam;
+            lstPersonen.appendChild(option);
+        }
+    }
 // onze setup functie die de event listeners registreert
     const setup = () => {
         let btnBewaar = document.getElementById("btnBewaar");
@@ -76,6 +117,7 @@ const bewaarBewerktePersoon = () => {
         let lstPersonen = document.getElementById("lstPersonen");
         // voeg een change listener toe aan lstPersonen. Bij het klikken op een option element in de lijst
         // moet de data van die persoon getoond worden in het formulier
+        lstPersonen.addEventListener('change',klikOptie);
+        showList();
     };
-
     window.addEventListener("load", setup);
